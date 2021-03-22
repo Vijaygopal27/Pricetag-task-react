31790593a01513fa35b1346c1fede8bd435f4e13
Vijaygopal27 <76892347+Vijaygopal27@users.noreply.github.com>
2021-03-22 17:01:22 +0530

    Add files via upload

---
+++ src/pricecard.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+
+function Pricecard(props){
+return (
+    <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">{props.details.plan}</h5>
+            <h6 class="card-price text-center">{props.details.currency}{props.details.price}<span class="period">/{props.details.period}</span></h6>
+            <hr/>
+            <ul class="fa-ul">
+              {
+                  props.details.listItems.map((item) => {
+                      return !item.isDisabled ? 
+                      <li><span class="fa-li"><i class="fas fa-check"></i></span>{item.itemName}</li> :
+                      <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>{item.itemName}</li>
+                  })
+              }
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+          </div>
+        </div>
+      </div>
+)
+}
+
+
+export default Pricecard;
+/*<li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited Private Projects</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Phone Support</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li> */
